6292ca7d4b666494569e7de85509c64b27fcdabc
emizack <emizack123@gmail.com>
2024-04-20 12:12:21 -0300

    primer cambio

---
+++ TP2/procesos.c
@@ -0,0 +1,10 @@
+<HTML>
+<HEAD>
+<TITLE>Moved Temporarily</TITLE>
+</HEAD>
+<BODY BGCOLOR="#FFFFFF" TEXT="#000000">
+<!-- GSE Default Error -->
+<H1>Moved Temporarily</H1>
+The document has moved <A HREF="https://drive.google.com/file/d/1qwN/">here</A>.
+</BODY>
+</HTML>
